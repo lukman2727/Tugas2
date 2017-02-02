3e7510eb6ad6e639f8d5277dc83bdf4baf0aa2eb
Lukman <campione47@gmail.com>
2017-02-02 22:59:37 +0700

    Membuat Luas Persegi Panjang

--- HitungLuas2.java
+++ HitungLuas2.java
@@ -1,14 +1,10 @@
-class HitungLuas2{
-	public static int cariLuasPersegiPanjang(int panjang, int lebar){
+class HitungLuas2 {
+
+	public static int cariLuasPersegiPanjang(int panjang, int lebar) {
 		return panjang * lebar;
 	}
-	public static float cariLuasLingkaran(int r){
-		return (22/7) * r * r;
-	
-	}
-	public static int cariLuasSegitiga(int a, int t){
-		return (a * t)/2;
-	}
-	public static void main (String[] args){
+
+	public static void main (String[] args) {
+
 	}
 }
