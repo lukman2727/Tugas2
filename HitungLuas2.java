a1ef611deb8151e4a5089c9a7ccdf12e08b7363f
Lukman <campione47@gmail.com>
2017-02-02 22:41:19 +0700

    Buat class HitungLuas2 dengan method main dan cariLuasPersegiPanjang

---
+++ HitungLuas2.java
@@ -0,0 +1,14 @@
+class HitungLuas2{
+	public static int cariLuasPersegiPanjang(int panjang, int lebar){
+		return panjang * lebar;
+	}
+	public static float cariLuasLingkaran(int r){
+		return (22/7) * r * r;
+	
+	}
+	public static int cariLuasSegitiga(int a, int t){
+		return (a * t)/2;
+	}
+	public static void main (String[] args){
+	}
+}
